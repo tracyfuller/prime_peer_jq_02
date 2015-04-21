16436f80dc9d3bb5022d73d16d33026ca1ada4ef
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-21 18:04:05 -0500

    Fixed slideUp, adjusted CSS styling

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-var propertyId;
-var squareFeet;
-var pricePerFoot;
+function generateProperty(){
+	var propertyId = randomNumber(1000, 9999);
+	var squareFeet = randomNumber(1000, 9999);
+	var pricePerFoot = randomNumber(5, 25);
+
+	var el = "<div class = 'row'><div class='propertyCell'>Property ID : " + propertyId + "</div><div class='feetCell'>" + squareFeet + " Sq. Ft.</div><div class='priceCell'>$" + pricePerFoot + " / sq.ft.</div><div class='buttonCell'> <button class='removeButton'>Remove</button></div></div>";
+
+	$("#ourTable").append(el);
+
+}
+
+function randomNumber(min, max) {
+		return Math.floor(Math.random() * (1+ max - min) + min);
+	}
 
 $(document).ready(function(){
 
 	$("#btn").on("click", function(){
-		propertyId = randomNumber(1000, 9999);
-		squareFeet = randomNumber(1000, 9999);
-		pricePerFoot = randomNumber(1, 25);
-
-
-
-
-		$('#ourTable').append("<div class = 'row'><div class='propertyCell'>Property ID : " + propertyId + "</div><div class='feetCell'>" + squareFeet + " Sq. Ft.</div><div class='priceCell'>$" + pricePerFoot + " / sq.ft.</div><div class='buttonCell'> <button class='removeButton'>Remove</button></div></div>");
-
-		var el = $('#ourTable').children().last();
-		el.hide().slideDown(1000);	
-
+		//using divs instead of tables made the code work. tables are old school, but they're still used so be aware whether or not jquery will break a table
+		generateProperty
+		var element = $('#ourTable').children().last();
+		element.hide().slideDown(1000);	
+		//.hide has to happen first, otherwise slideDown wouldn't work. 
 	});
 
 	$('#ourTable').on('click', '.removeButton', function(){
-		$(this).parents('.row').remove();
+		//note that I couldn't use .hide() here, because it broke the toggle. It tried to show something hidden which is jumpy and unpleasant
+		$(this).parents('.row').slideToggle(1000, function(){
+			$(this).remove();
+		});
 	});
-
-
-
-	function randomNumber(min, max) {
-		return Math.floor(Math.random() * (1+ max - min) + min);
-	}
-
+	//advanced selector: .newPropert button <  notice that there's a space
 });
+
+//TODO:
+//find something cool in jquery documentation and come ready to talk about it. pick something that you understand, because you will be asked questions
+//also, wrap up this project
